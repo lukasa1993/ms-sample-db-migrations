6fbdb39d18afda3713ccc475112f052a6929ebb7
luka dodelia <luka@picktek.com>
2022-08-19 18:42:08 +0400

    company placeholder

--- migrations/sqls/20220817065153-init-up.sql
+++ migrations/sqls/20220817065153-init-up.sql
@@ -1,16 +1,37 @@
 /* Replace with your SQL commands */
 
-CREATE EXTENSION pgcrypto;
+CREATE
+EXTENSION IF NOT EXISTS pgcrypto;
 
 drop table IF EXISTS "Users";
+drop table IF EXISTS "Company";
 
-create table "Users"
+create table "Company"
 (
     uuid    uuid                     default gen_random_uuid() not null,
     name    varchar                                            not null,
     email   varchar                                            not null,
+    meta    jsonb                    default '{}'              not null,
     created timestamp with time zone default now()             not null,
     updated timestamp with time zone default now()             not null
 );
 
-insert into "Users"("name", "email") values ('demo', 'demo@example.com');
+create table "Users"
+(
+    uuid         uuid                     default gen_random_uuid() not null,
+    company_uuid uuid                                               not null,
+    name         varchar                                            not null,
+    email        varchar                                            not null,
+    code         varchar null,
+    meta         jsonb                    default '{}'              not null,
+    created      timestamp with time zone default now()             not null,
+    updated      timestamp with time zone default now()             not null
+);
+
+insert into "Company"("name", "email")
+values ('LLC Demo', 'support@example.com');
+
+insert into "Users"("company_uuid", "name", "email")
+select uuid as "company_uuid", 'demo' as "name", 'demo@example.com' as "email"
+from "Company"
+where email = 'support@example.com';
